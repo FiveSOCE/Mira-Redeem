e67a69e9b8fad51616a73cf5ff2762be62937f22
FiveS <sgmmsionide@gmail.com>
2026-09-06 05:13:49 +1000

    Extend redeem definitions for rank vouchers

--- src/main/java/com/mira/redeem/model/RedeemDefinition.java
+++ src/main/java/com/mira/redeem/model/RedeemDefinition.java
@@ -6,10 +6,13 @@ import java.util.List;
 
 public record RedeemDefinition(
         String id,
+        RedeemType type,
         Material material,
         String name,
         List<String> lore,
         List<String> commands,
-        String successMessage
+        String successMessage,
+        String track,
+        String targetGroup
 ) {
 }
