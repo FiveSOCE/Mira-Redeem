96d2e6e2216bbc250974763982c9a501b2bcca66
FiveS <sgmmsionide@gmail.com>
2026-09-09 06:01:39 +1000

    Add 1.21.11-26.2 compatibility build for MiraRedeem 0.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,25 +3,29 @@ plugins {
 }
 
 group = "com.mira"
-version = "0.1.2"
+version = "0.1.3"
 
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
 }
 
+val paperApiVersion = providers.gradleProperty("paperApiVersion").orElse("1.21.11-R0.1-SNAPSHOT")
+val compileJavaVersion = providers.gradleProperty("compileJavaVersion").map(String::toInt).orElse(21)
+val bytecodeJavaVersion = providers.gradleProperty("bytecodeJavaVersion").map(String::toInt).orElse(21)
+
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:${paperApiVersion.get()}")
     compileOnly("net.luckperms:api:5.5")
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(compileJavaVersion.get()))
 }
 
 tasks.withType<JavaCompile>().configureEach {
     options.encoding = "UTF-8"
-    options.release.set(21)
+    options.release.set(bytecodeJavaVersion.get())
 }
 
 tasks.jar {
